134978f937a3423c0e75a828d55b975491019465
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-09-13 16:01:28 +0200

    Add project files.

---
+++ upg 312/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace upg_312
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hur många datorer har du");
+           int numDat = int.Parse(Console.ReadLine());
+
+            if (numDat == 1) 
+            {
+                Console.WriteLine("Du har 1 dator");
+            }
+            else
+            {
+                Console.WriteLine($"du har {numDat} datorer");
+            }
+        }
+    }
+}
